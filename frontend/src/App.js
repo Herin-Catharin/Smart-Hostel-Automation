94aba1a07a0ff9abb6bf822f17949a039081dc4a
mairu0087 <jenishlinb@gmail.com>
2026-01-23 23:08:53 +0530

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import herin from './assets/herin.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={herin} className="App-logo" alt="Herin" />
+        
+        <p>NAA THAN HERIN CATHARIN 😎</p>
+
       </header>
     </div>
   );
